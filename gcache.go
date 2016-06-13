1cc21094247e6797b1ef91e86dc578075a6fb4da
yuanyangen <yuanyangen@gmail.com>
2016-06-14 00:43:57 +0800

    not finished but i wanna to sleep

--- gcache.go
+++ gcache.go
@@ -8,12 +8,9 @@ import (
 //this package implement a cache simuliar to memcached, it use 2Q  as its evict method
 type cache struct {
 	data       map[string]*Item
-	queue1     *QueueMeta //the meta data of the first queue
-	queue2     *QueueMeta // the meta data of the second queue
 	deleteChan chan *Item //
-	moveChan chan *Item //
 	rwLock     sync.RWMutex
-	lruK int64 //
+	lru *Lru
 }
 
 
@@ -21,36 +18,33 @@ type Item struct {
 	key        string
 	value      interface{}
 	expiration int64 //second of expiration , 0 means never expire
-	prev       *Item
-	next       *Item
 	rwLock     sync.RWMutex
-	queueNo    int   //indicate queue this item in
 	refCount int64 //引用次数
+	node *Node
 }
-
-const NOQueue1 = 1
-const NOQueue2 = 2
 
 var Gcache = &cache{}
 
 //init the hash map and the two queue
 func init() {
 	Gcache.data = make(map[string]*Item)
-	Gcache.lruK = 1
-	Gcache.moveChan = make(chan *Item, 1024)
 	Gcache.deleteChan = make(chan *Item, 1024)
-	Gcache.queue1 = &QueueMeta{id:NOQueue1, MaxLen:102400}
-	Gcache.queue2 = &QueueMeta{id:NOQueue2, MaxLen:102400}
-	Gcache.queue1.Len = 0
-	Gcache.queue1.addChan = make(chan *Item, 10240)
-	Gcache.queue1.delChan = make(chan *Item, 10240)
-	Gcache.queue2.Len = 0
-	Gcache.queue2.addChan = make(chan *Item, 10240)
-	Gcache.queue2.delChan = make(chan *Item, 10240)
-	Gcache.queue1.daemonPutItemToHead()
-	Gcache.queue2.daemonPutItemToHead()
+	Gcache.lru.delChan = make(chan *Node, 1024)
+	Gcache.lru.getChan = make(chan *Node, 1024)
+	Gcache.lru.setChan = make(chan *Node, 1024)
+	Gcache.lru.queue1 = &Queue{id:NOQueue1, MaxLen:102400}
+	Gcache.lru.queue2 = &Queue{id:NOQueue2, MaxLen:102400}
+	Gcache.lru.queue1.Len = 0
+	Gcache.lru.queue2.Len = 0
+	/*
+	Gcache.lru.queue1.addChan = make(chan *Node, 10240)
+	Gcache.lru.queue1.delChan = make(chan *Node, 10240)
+	Gcache.lru.queue2.addChan = make(chan *Node, 10240)
+	Gcache.lru.queue2.delChan = make(chan *Node, 10240)
+	*/
+	Gcache.lru.daemonA()
+	Gcache.lru.lruK = 1
 	Gcache.daemonDelete()
-	Gcache.daemonMoveItem()
 }
 
 //get the ptr of the item we want to operate
@@ -65,12 +59,16 @@ func (c *cache)getItem(key string) *Item {
 	//here seems a little strange , the condition below is for thread safe
 	//todo may be i can optimize this
 	c.rwLock.Lock()
+
+	// make this is concurrency safe
 	if item, ok := c.data[key]; ok {
 		c.rwLock.Unlock()
 		return item
 	}
-
-	c.data[key] = &Item{key:key, expiration:0}
+	node := &Node{}
+	item := &Item{key:key, expiration:0, node:node}
+	node.item = item
+	c.data[key] = item
 	c.rwLock.Unlock()
 	return c.data[key]
 }
@@ -82,7 +80,7 @@ func (c *cache)Set(key string, value interface{}, expiration int64) error {
 	item.value = value
 	item.expiration = int64(time.Now().Unix()) + expiration
 	item.rwLock.Unlock()
-	c.queue1.addChan <- item
+	c.lru.setChan <- item.node
 	return nil
 }
 
@@ -93,21 +91,7 @@ func (c *cache)Get(key string)(interface{}) {
 	if int64(time.Now().Unix()) > item.expiration {
 		return nil
 	}
-
-	//remove from the first queue, then add to the second queue
-	if item.refCount >= c.lruK{
-		c.moveChan <- item
-	} else if item.refCount > c.lruK{
-		c.queue2.addChan <- item
-	} else { //add this to queue1
-		c.queue1.addChan <- item
-	}
-	item.rwLock.Lock()
-	item.refCount++
-	item.rwLock.Unlock()
-
-
-	//add to the head of the second queue
+	c.lru.getChan <- item.node
 	return item.value
 }
 
@@ -163,23 +147,9 @@ func (c *cache) daemonDelete() {
 		for {
 			item := <-c.deleteChan
 			if _, ok := c.data[item.key]; ok {
-				if item.queueNo == NOQueue1 {
-					c.queue1.delChan <- item
-				} else if item.queueNo == NOQueue2 {
-					c.queue2.delChan <- item
-				}
+				c.lru.delChan <- item.node
 				delete(c.data, item.key)
 			}
-		}
-	}()
-}
-
-func (c *cache) daemonMoveItem() {
-	go func() {
-		for {
-			item := <- c.moveChan
-			removeItemFromQueue(c.queue1, item)
-			putItemToHead(c.queue2, item)
 		}
 	}()
 }
---
+++ lru.go
@@ -0,0 +1,158 @@
+package gcache
+
+type Lru struct {
+	queue1  *Queue
+	queue2  *Queue
+	lruK    int64      //
+	getChan chan *Node //
+	setChan chan *Node //
+	delChan chan *Node //
+}
+
+type Queue struct {
+	id     int
+	Len    int64
+	MaxLen int64
+	Head   *Node
+	Tail   *Node
+}
+
+type Node struct {
+	prev     *Node
+	next     *Node
+	item     *Item
+	queueNo  int
+	refCount int64
+}
+
+const NOQueue1 = 1
+const NOQueue2 = 2
+
+
+// all operation to the lru queue are being serialized to make sure the operation is concurrency safe
+func (l *Lru)daemonA() {
+	/*
+	go func() {
+		for {
+			select {
+			case node := <-l.queue1.addChan:
+				putnodeToHead(l.queue1, node)
+			case node := <-l.queue2.addChan:
+				putnodeToHead(l.queue2, node)
+			case node := <-l.queue1.delChan:
+				removenodeFromQueue(l.queue1, node)
+			case node := <-l.queue2.delChan:
+				removenodeFromQueue(l.queue2, node)
+			case node := <-l.moveChan:
+
+				removenodeFromQueue(l.queue1, node)
+				putnodeToHead(l.queue2, node)
+			}
+		}
+	}()
+*/
+	go func() {
+		for {
+			select {
+			case node := <-l.getChan:
+				{
+					if node.refCount == l.lruK {
+						removeNodeFromQueue(l.queue1, node)
+						putNodeToHead(l.queue2, node)
+					} else if node.refCount > l.lruK {
+						putNodeToHead(l.queue2, node)
+					} else {
+						putNodeToHead(l.queue1, node)
+					}
+					node.refCount++
+				}
+			case node := <-l.setChan:
+				putNodeToHead(l.queue1, node)
+			//todo may be bugs, node may not in this queue
+				removeNodeFromQueue(l.queue2, node)
+			case node := <-l.delChan:
+				{
+					if node.queueNo == NOQueue1 {
+						removeNodeFromQueue(l.queue1, node)
+					} else if node.queueNo == NOQueue2 {
+						removeNodeFromQueue(l.queue2, node)
+					}
+				}
+			}
+
+			//receive the command from get , set and delete , then do it
+
+			//remove from the first queue, then add to the second queue
+
+
+		}
+	}()
+}
+
+func putNodeToHead(q *Queue, node *Node) {
+	// if node already in this queue
+	if node.queueNo == q.id {
+		//if node already in the head of the queue
+		if q.Head == node {
+			return
+		}
+		node.next = q.Head
+		q.Head = node
+
+		if node != q.Tail {
+			// if node not in the tail of queue
+			node.prev.next = node.next
+			node.next.prev = node.prev
+
+		} else {
+			// node at the tail of the queue
+			node.prev.next = nil
+			q.Tail = node.prev
+		}
+	} else {
+		//if node not in this queue
+		//if queue is empty
+		node.queueNo = q.id
+		if q.Head == nil && q.Tail == nil {
+			q.Head = node
+			q.Tail = node
+			q.Len = 1
+		} else {
+			//if queue is not empty
+			node.next = q.Head
+			q.Head = node
+			q.Len++
+		}
+	}
+}
+
+func removeNodeFromQueue(q *Queue, node *Node) {
+	if node.queueNo != q.id {
+		return
+	}
+	//if only one node in this queue
+	if node.next == nil && node.prev == nil {
+		q.Head = nil
+		q.Tail = nil
+		return
+	}
+
+	//if the current node is the first one of this queue
+	if node.prev == nil {
+		q.Head = node.next
+		node.next.prev = nil
+	} else if node.next == nil {
+		//if the current node is the last one of this queue
+		q.Tail = node.prev
+		node.prev.next = nil
+	} else {
+		// this node in the mid of this queue
+		node.prev.next = node.next
+		node.next.prev = node.prev
+	}
+
+	q.Len--
+	node.next = nil
+	node.prev = nil
+}
+
